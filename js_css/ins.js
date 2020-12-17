26229b9685daf0c6487a9cdb18689282722680f8
天境云生 <tjys@qikaile.onmicrosoft.com>
2020-12-17 10:15:43 +0800

    Update ins.js

--- js_css/ins.js
+++ js_css/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           // 需要修改https://raw.githubusercontent.com/你的github名/你的github仓库名/master/min_photos/即可
-          var minSrc = 'https://cdn.jsdelivr.net/gh/Qikaile/img/min_photos/' + data.link[i];
-          var src = 'https://cdn.jsdelivr.net/gh/Qikaile/img/photos/' + data.link[i];
+          var minSrc = 'https://img.tjys.ml/min_photos/' + data.link[i];
+          var src = 'https://img.tjys.ml/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
